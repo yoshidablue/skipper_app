d9170c6564a9f1a5b812edd100c87ac90c9f1e44
yoshidablue <sugar.blue.1224@gmail.com>
2022-11-04 05:16:49 +0000

    jQuery

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,2 +1,12 @@
+/* global $*/
+// 上記を記述することでこのページでのエラーが出なくなる。（書かなくても問題なし）
+
 let title = "javascriptが使えました";
 alert(title);
+
+ // 下記の記述でエラーが出るが上記の/* global $*/でエラーが出なくなる。（動作には問題なし）
+$(document).ready(function () {
+ $('.jquery').on('click', function(){
+   $(this).css('color','red');
+ });
+});
